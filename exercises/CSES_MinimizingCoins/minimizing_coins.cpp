60bb9784517001da619c8494ffe904b8a2fec687
diogjunior100 <diogjunior10071@gmail.com>
2023-07-09 19:23:21 -0300

    modifica localizacao da pasta

---
+++ exercises/CSES_MinimizingCoins/minimizing_coins.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using ll = long long int;
+using namespace std;
+
+ll solve(vector<ll> coins, ll n, ll x){
+    vector<ll> dp(x+10);
+
+    for (ll i = 1; i <= x; i++)
+    {
+        dp[i] = INT_MAX;
+
+        for (ll j = 0; j < n; j++)
+        {
+            if ((i - coins[j]) >= 0)
+            {
+                dp[i] = min(dp[i],dp[i-coins[j]]+1);
+            }   
+        }
+    }
+    
+    return dp[x] >= INT_MAX ? -1 : dp[x];
+}
+
+
+int main()
+{
+    ll n, x, temp;
+
+    cin >> n >> x;
+    
+    vector<ll> coins(n+1);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> temp;
+        coins.push_back(temp);
+    }
+
+    cout << solve(coins, x, n) << endl;
+    
+    return 0;
+}
